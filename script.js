8432bc949d7b0634d4f3b8a01c3b8060e3911169
Abdullah <abdullahansir8644@gmail.com>
2024-12-21 16:38:40 +0500

    Created getsongs Function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("JavaScript is working!");
+
+async function getsongs() {
+
+
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = await response;
+    let as = div.getElementsByTagName("a");
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+
+    return songs
+
+
+}
+getsongs();
