552da9da4a245641a4cc39fb20a2fbb43ba59999
Abdullah <abdullahansir8644@gmail.com>
2024-12-22 18:06:03 +0500

    Use more Python

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 songs=os.listdir("songs")
 with open("songs.txt" ,"w") as f:
     pass
@@ -7,4 +6,23 @@ with open("songs.txt" ,"w") as f:
 for i in songs:
     with open("songs.txt" ,"a") as f:
         f.write(f"<li><a href=\"songs/{i}\"></a></li>\n")
+    
+with open("songs.txt","r") as f:
+    songs=(f.read())
+    with open("songs.html","w") as h:
+        html=f"""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>songs</title>
+  </head>
+  <body>
+    <ul>
+    {songs}
+    </ul>
+  </body>
+</html>
+"""
+        h.write(html)
     
